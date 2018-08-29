8f53d08cd7079461a2c4e95406b3553ed53bbb48
kyu-suke <ykaneoka7@gmail.com>
2018-08-29 15:16:24 +0900

    modfileName

--- init.sh
+++ init.sh
@@ -1,8 +1,8 @@
-ln -sf ~/dotfiles/.bash_profile   ~/.bash_profile
-ln -sf ~/dotfiles/.bashrc         ~/.bashrc
+ln -sf ~/dotfiles/bash_profile   ~/.bash_profile
+ln -sf ~/dotfiles/bashrc         ~/.bashrc
 ln -sf ~/dotfiles/.git            ~/.git
-ln -sf ~/dotfiles/.tmux.conf      ~/.tmux.conf
-ln -sf ~/dotfiles/.vim            ~/.vim
-ln -sf ~/dotfiles/.vimrc          ~/.vimrc
+ln -sf ~/dotfiles/tmux.conf      ~/.tmux.conf
+ln -sf ~/dotfiles/vim            ~/.vim
+ln -sf ~/dotfiles/vimrc          ~/.vimrc
 
 
