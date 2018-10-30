ef429dd4762fc1c4563f5ec52e5cf8da02ed2b7b
kyu-suke <ykaneoka7@gmail.com>
2018-10-30 12:42:21 +0900

    gitconfig,install

--- install.sh
+++ install.sh
@@ -6,4 +6,6 @@ phpenv/bin/phpenv-install.sh
 curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash > ./git-completion.bash
 echo "complete -o bashdefault -o default -o nospace -F _git g 2>/dev/null \ || complete -o default -o nospace -F _git g" >> git-completion.bash
 
+pip3 install icdiff
+pip3 install diff-highlight
 
