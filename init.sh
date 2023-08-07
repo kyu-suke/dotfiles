6a029db94061930006bf1b7787f99ab4f00de8d8
kyu-suke <ykaneoka7@gmail.com>
2023-08-07 14:40:19 +0900

    setupsh

--- init.sh
+++ init.sh
@@ -2,19 +2,16 @@ ls -al ~/
 
 mkdir -p ~/.config/nvim/
 
-unlink ~/.bash_profile
 unlink ~/.bashrc
 unlink ~/.tmux.conf
 unlink ~/.vim
 unlink ~/.vimrc
 unlink ~/.gitconfig
 unlink ~/.gvimrc
-unlink ~/.ideavimrc
 unlink ~/.inputrc
 unlink ~/.config/nvim/init.vim
 
 
-ln -sf ~/dotfiles/bash_profile ~/.bash_profile
 ln -sf ~/dotfiles/bashrc       ~/.bashrc
 ln -sf ~/dotfiles/tmux.conf    ~/.tmux.conf
 ln -sf ~/dotfiles/vim          ~/.vim
@@ -22,7 +19,6 @@ ln -sf ~/dotfiles/vimrc        ~/.vimrc
 ln -sf ~/dotfiles/vimrc        ~/.config/nvim/init.vim
 ln -sf ~/dotfiles/gitconfig    ~/.gitconfig
 ln -sf ~/dotfiles/gvimrc       ~/.gvimrc
-ln -sf ~/dotfiles/ideavimrc    ~/.ideavimrc
 ln -sf ~/dotfiles/inputrc      ~/.inputrc
 
 ls -al ~/
--- install.sh
+++ install.sh
@@ -1,36 +1,15 @@
-# xcode-select --install
+# install home brew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" &&
 
-brew install python3
-brew install rbenv
-brew install grep
-
-# brew install re2c
-# brew install openssl
-# brew install bison
-# brew install libxml2
-# brew install autoconf
-# brew install automake
-# brew install icu4c
-# brew install libjpeg
-# brew install libpng
-# brew install libmcrypt
-
-# git clone https://github.com/CHH/phpenv.git
-# phpenv/bin/phpenv-install.sh
-
-# git clone https://github.com/CHH/php-build.git ~/.phpenv/plugins/php-build
+# brew install
+brew install git grep
+brew install --cask obsidian kindle visual-studio-code brave-browser google-chrome arc clipy discord orbstack git grep
 
 # curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash > ./git-completion.bash
 # echo "complete -o bashdefault -o default -o nospace -F _git g 2>/dev/null \ || complete -o default -o nospace -F _git g" >> git-completion.bash
 
-pip3 install icdiff
-pip3 install diff-highlight
-
-
 # brew cask install homebrew/cask-versions/sequel-pro-nightly
 
 source ./bashrc
-# curl -fsSL https://github.com/rbenv/rbenv-installer/raw/master/bin/rbenv-doctor | bash
-# brew upgrade rbenv ruby-build
 
 
