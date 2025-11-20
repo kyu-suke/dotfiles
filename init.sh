b683c46976a7dddeca2c0c8961061961f63a8838
kyu-suke <ykaneoka7@gmail.com>
2025-11-20 20:15:10 +0900

    rm:nvim

--- init.sh
+++ init.sh
@@ -1,7 +1,5 @@
 ls -al ~/
 
-mkdir -p ~/.config/nvim/
-
 unlink ~/.bashrc
 unlink ~/.zshrc
 unlink ~/.tmux.conf
@@ -10,15 +8,12 @@ unlink ~/.vimrc
 unlink ~/.gitconfig
 unlink ~/.gvimrc
 unlink ~/.inputrc
-unlink ~/.config/nvim/init.vim
-
 
 ln -sf ~/dotfiles/bashrc       ~/.bashrc
 ln -sf ~/dotfiles/zshrc        ~/.zshrc
 ln -sf ~/dotfiles/tmux.conf    ~/.tmux.conf
 ln -sf ~/dotfiles/vim          ~/.vim
 ln -sf ~/dotfiles/vimrc        ~/.vimrc
-ln -sf ~/dotfiles/vimrc        ~/.config/nvim/init.vim
 ln -sf ~/dotfiles/gitconfig    ~/.gitconfig
 ln -sf ~/dotfiles/gvimrc       ~/.gvimrc
 ln -sf ~/dotfiles/inputrc      ~/.inputrc
